71a82cfc4bac1b86ad52720ef784da7d9c37c302
LiYuanhao <liyuanhao@zju.edu.cn>
2020-10-12 09:33:03 +0800

    add getting system time function, OS delay function

--- SRC/DRIVER/delay.h
+++ SRC/DRIVER/delay.h
@@ -7,5 +7,9 @@ void DelayInit(u8 SYSCLK);
 void DelayUs(u32 nus);
 void DelayMs(u32 nms);
 void DelayXms(u32 nms);
+void OsDelayMs(uint32_t ms);
+
+uint64_t GetSysTimeUs(void);
+uint32_t GetSysTimeMs(void);
 
 #endif
