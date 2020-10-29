30afc09dbfe0f466903629a657418fac22cb7d5f
LiYuanhao <liyuanhao@zju.edu.cn>
2020-10-29 19:00:29 +0800

    rewrite test module for clock,flash and math tool, combined with OS

--- SRC/MODULE/rgb.h
+++ SRC/MODULE/rgb.h
@@ -5,4 +5,14 @@
 void RGB_Init(void);
 void RGB_Flash(void);
 void Red_Flash(void);
+
+void RGB_Green_Off(void);
+void RGB_Green_On(void);
+void RGB_Red_Off(void);
+void RGB_Red_On(void);
+void RGB_Blue_Off(void);
+void RGB_Blue_On(void);
+void LED_Red_On(void);
+void LED_Red_Off(void);
+
 #endif
